1f044e554fae36305262c615533002f9d19f0cbf
XiaoBaoJun <xbj1525@126.com>
2020-03-22 16:21:03 +0800

    环境的配置

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,17 @@
 </template>
 
 <script>
+import storage from './storage'
 export default {
   name: 'App',
   components: {
+  },
+  mounted(){
+    console.log(1)
+    // storage.setItem('a',3)
+    // storage.setItem('user',{a:1})
+    // storage.setItem('abc',{a:2},'user')
+    storage.clear('a','user')
   }
 }
 </script>
